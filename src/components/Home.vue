842eb3f43e7c1ec9954e35a3f46d5f8d2f246004
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2021-09-04 17:29:58 +0430

    Restaurants api created

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,37 @@
 <template>
 <Header />
 <h1>Hello {{ name }} Welcome to home page.</h1>
+<table border="1px">
+    <thead>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Contact</td>
+        <td>Address</td>
+    </thead>
+    <tr v-for="item in restaurants" :key="item.id">
+        <td>{{item.id}}</td>
+        <td>{{item.name}}</td>
+        <td>{{item.contact}}</td>
+        <td>{{item.address}}</td>
+    </tr>
+</table>
 </template>
 
 <script>
+import axios from 'axios';
 import Header from './Header.vue'
 export default {
     name: "Home",
     data() {
         return {
-            name: ''
+            name: '',
+            restaurants: []
         }
     },
     components: {
         Header
     },
-    mounted() {
+    async mounted() {
         let user = localStorage.getItem("user-info");
         // this.name = JSON.parse(user).name;
         if (!user) {
@@ -23,6 +39,19 @@ export default {
                 name: "Register"
             });
         }
+        let result = await axios.get("http://localhost:3000/restaurants");
+        this.restaurants = result.data;
     },
 }
 </script>
+
+<style scoped>
+table {
+    border-collapse: collapse;
+}
+
+td {
+    width: 160px;
+    height: 40px;
+}
+</style>
